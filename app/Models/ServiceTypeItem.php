ab38d78e4562cfb12a5ae1cd064bc924b754237a
chhatraman shrestha <cms111000111@gmail.com>
2021-05-02 15:41:02 +0545

    service item update

--- app/Models/ServiceTypeItem.php
+++ app/Models/ServiceTypeItem.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceTypeItem extends Model
 {
     use HasFactory;
+    public function service(){
+        return $this->belongsTo(ServiceType::class,'service_type_id');
+    }
 }
