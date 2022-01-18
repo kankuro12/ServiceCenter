83a87f86246e277eef0bda770575628e37c1f808
chhatraman shrestha <cms111000111@gmail.com>
2022-01-18 08:42:32 +0545

    seeder destroyed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,16 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(FooterSeeder::class);
-        $this->call(FooterinfoSeeder::class);
-        $this->call(SocialMediaSeeder::class);
-        $this->call(DistrictSeeder::class);
-        $this->call(BannerSeeder::class);
-        $this->call(SliderSeeder::class);
-        $this->call(AboutSeeder::class);
-        $this->call(TermsSeeder::class);
-        $this->call(HomeinfoSeeder::class);
-        $this->call(PopupSeeder::class);
+        // $this->call(FooterSeeder::class);
+        // $this->call(FooterinfoSeeder::class);
+        // $this->call(SocialMediaSeeder::class);
+        // $this->call(DistrictSeeder::class);
+        // $this->call(BannerSeeder::class);
+        // $this->call(SliderSeeder::class);
+        // $this->call(AboutSeeder::class);
+        // $this->call(TermsSeeder::class);
+        // $this->call(HomeinfoSeeder::class);
+        // $this->call(PopupSeeder::class);
         // $this->call(UserSeeder::class);
     }
 }
