0fc769a572e028d2d1a38314c8931d848e198c0e
chhatraman shrestha <cms111000111@gmail.com>
2022-01-18 08:46:44 +0545

    front page updated

--- resources/views/Need/home/index.blade.php
+++ resources/views/Need/home/index.blade.php
@@ -27,34 +27,34 @@
     <div id="section">
         <div class="row m-0">
             {{-- <div class="col-md-6 p-0"> --}}
-                <div class="work">
-                    <div class="section-inner ">
-                        <div class="section-title">
-                            Work
-                        </div>
-                        <div class="section-desc">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam similique itaque doloribus incidunt expedita perspiciatis ad non, dolorum accusantium reprehenderit quam facilis nostrum eaque cum ipsam inventore vel, sint dicta!
-                        </div>
-                        <a href="{{route('n.front.all-category')}}" class="section-btn">
-                            Visit
-                        </a>
+            <div class="work">
+                <div class="section-inner ">
+                    <div class="section-title">
+                        Work
                     </div>
+                    <div class="section-desc">
+                        {{ custom_config('fj_short')->value ?? '' }}
+                    </div>
+                    <a href="{{ route('n.front.all-category') }}" class="section-btn">
+                        Visit
+                    </a>
                 </div>
+            </div>
             {{-- </div> --}}
             {{-- <div class="col-md-6 p-0"> --}}
-                <div class="ride">
-                    <div class="section-inner ">
-                        <div class="section-title">
-                            Ride
-                        </div>
-                        <div class="section-desc">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam similique itaque doloribus incidunt expedita perspiciatis ad non, dolorum accusantium reprehenderit quam facilis nostrum eaque cum ipsam inventore vel, sint dicta!
-                        </div>
-                        <a class="section-btn" href="{{route('n.front.book.step1')}}">
-                            Visit
-                        </a>
+            <div class="ride">
+                <div class="section-inner ">
+                    <div class="section-title">
+                        Ride
                     </div>
+                    <div class="section-desc">
+                        {{ custom_config('bs_short')->value ?? '' }}
+                    </div>
+                    <a class="section-btn" href="{{ route('n.front.book.step1') }}">
+                        Visit
+                    </a>
                 </div>
+            </div>
             {{-- </div> --}}
         </div>
     </div>
@@ -169,32 +169,34 @@
 
     </div>
   </div> --}}
-@include('Need.home.popup')
+    @include('Need.home.popup')
 @endsection
 @section('css')
     <link rel="stylesheet" href="{{ asset('assets\css\front.css') }}">
     <style>
-        #popup .modal-content{
+        #popup .modal-content {
             border: none;
             border-radius: 0px;
         }
-        /* .main-banner {
-            padding: 125px 50px 0px 50px;
-        }
-        @media (max-width:426px){
-            .main-banner {
-            padding: 125px 0px 0px 0px;
-        }
 
-        } */
+        /* .main-banner {
+                padding: 125px 50px 0px 50px;
+            }
+            @media (max-width:426px){
+                .main-banner {
+                padding: 125px 0px 0px 0px;
+            }
+
+            } */
+
     </style>
 @endsection
 @section('js')
     <script>
-         var cats={!! json_encode($cats) !!};
-        const catele=document.getElementById('cats');
-        const jobele=document.getElementById('job-container');
-                $('#slid').owlCarousel({
+        var cats = {!! json_encode($cats) !!};
+        const catele = document.getElementById('cats');
+        const jobele = document.getElementById('job-container');
+        $('#slid').owlCarousel({
             rtl: true,
             loop: true,
             margin: 10,
@@ -202,52 +204,58 @@
             nav: true,
             items: 1
         })
-        $(document).ready(function () {
-            let ismobile=window.innerWidth <426;
-            let url="{{route('n.front.view-job',['job'=>'xxx_id'])}}";
-            let caturl="{{route('n.front.job-category',['cat'=>'xxx_id'])}}";
-            if(ismobile){
+        $(document).ready(function() {
+            let ismobile = window.innerWidth < 426;
+            let url = "{{ route('n.front.view-job', ['job' => 'xxx_id']) }}";
+            let caturl = "{{ route('n.front.job-category', ['cat' => 'xxx_id']) }}";
+            if (ismobile) {
                 cats.forEach(cat => {
-                    catele.innerHTML+='<li class="single-cat" id="single-cat-'+cat.i+'" data-id="'+cat.i+'" data-name="'+cat.n+'">'+
-                    '<a href="'+(caturl.replace('xxx_id',cat.i))+'">'+ cat.n+'</a>'+
-                    '</li>';
+                    catele.innerHTML += '<li class="single-cat" id="single-cat-' + cat.i + '" data-id="' +
+                        cat.i + '" data-name="' + cat.n + '">' +
+                        '<a href="' + (caturl.replace('xxx_id', cat.i)) + '">' + cat.n + '</a>' +
+                        '</li>';
 
                 });
-            }else{
+            } else {
                 cats.forEach(cat => {
-                    catele.innerHTML+='<li class="single-cat" id="single-cat-'+cat.i+'" data-id="'+cat.i+'" data-name="'+cat.n+'">'+cat.n+'</li>';
-                    html='<div class="row d-none cat" id="cats-'+cat.i+'">';
-                        cat.j.forEach(job => {
-                            let d=job.sdata.split("|");
-                            html+='<div class="col-md-4  " id="cat-'+d[0]+'">'+
-                            '<a href="'+(url.replace('xxx_id',d[0]))+'" class="single-cat-view  ">'+
-                            '<div class="job-name nowrap">'+d[1]+'</div>'+
-                            '<div class="job-company nowrap">'+d[3]+'</div>'+
-                            '<div class="job-duedate"> <span>Due Date</span><span>'+d[2]+'</span></div>'+
-                            '</a>'+
+                    catele.innerHTML += '<li class="single-cat" id="single-cat-' + cat.i + '" data-id="' +
+                        cat.i + '" data-name="' + cat.n + '">' + cat.n + '</li>';
+                    html = '<div class="row d-none cat" id="cats-' + cat.i + '">';
+                    cat.j.forEach(job => {
+                        let d = job.sdata.split("|");
+                        html += '<div class="col-md-4  " id="cat-' + d[0] + '">' +
+                            '<a href="' + (url.replace('xxx_id', d[0])) +
+                            '" class="single-cat-view  ">' +
+                            '<div class="job-name nowrap">' + d[1] + '</div>' +
+                            '<div class="job-company nowrap">' + d[3] + '</div>' +
+                            '<div class="job-duedate"> <span>Due Date</span><span>' + d[2] +
+                            '</span></div>' +
+                            '</a>' +
                             '</div>';
-                        });
-                    html+= "<div class='py-2'><a class='btn btn-primary' href='"+(caturl.replace('xxx_id',cat.i))+"'>View More In "+cat.n+"</a></div></div>";
+                    });
+                    html += "<div class='py-2'><a class='btn btn-primary' href='" + (caturl.replace(
+                        'xxx_id', cat.i)) + "'>View More In " + cat.n + "</a></div></div>";
                     // console.log(html);
-                    jobele.innerHTML+=html;
+                    jobele.innerHTML += html;
                 });
-                $('.single-cat').click(function (e) {
+                $('.single-cat').click(function(e) {
                     e.preventDefault();
-                    activate(this.dataset.id,this.dataset.name);
+                    activate(this.dataset.id, this.dataset.name);
                 });
-                activate(cats[0].i,cats[0].n);
+                activate(cats[0].i, cats[0].n);
 
             }
 
             $('#popup').modal('show');
         });
-            function activate(id,name){
-                $('.cat').addClass('d-none');
-                $('#cats-'+id).removeClass('d-none');
-                $('.single-cat').removeClass('active');
-                $('#single-cat-'+id).addClass('active');
-                $('#job-category-name').html(name);
-            }
+
+        function activate(id, name) {
+            $('.cat').addClass('d-none');
+            $('#cats-' + id).removeClass('d-none');
+            $('.single-cat').removeClass('active');
+            $('#single-cat-' + id).addClass('active');
+            $('#job-category-name').html(name);
+        }
     </script>
 
 @endsection
